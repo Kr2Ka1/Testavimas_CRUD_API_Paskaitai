20763a10b3c9600b9fb5221f0939915848f8ff48
Kr2Ka1 <kriste.kore.flora@gmail.com>
2025-02-06 09:37:32 +0200

    duombaziuBackup

---
+++ POSTMAN/productsnd_202502060825.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.productsnd (title,description,price) VALUES
+	 ('lempa','staline lempa',15.99),
+	 ('sofa','kamoine soda',2030.0);
---
+++ POSTMAN/users_202502051046.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.users (id,username,"password") VALUES
+	 (1,'test','test'),
+	 (3,'test3','test3'),
+	 (2,'test2','test2'),
+	 (4,'test4','test4'),
+	 (1000,'idetasPerInsert','idetasPerInsert');
